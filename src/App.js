c6c749a85b483ca0598c3d4935f56c095088a4de
hai14894 <hohoanghai14894@gmail.com>
2020-08-12 10:57:24 +1000

    add post form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Posts from './components/Posts'
+import PostForm from './components/PostForm'
 const App = () => {
   return (
     <div>
+      <PostForm/>
+      <hr/>
       <Posts/>
+      
     </div>
   )
 }
